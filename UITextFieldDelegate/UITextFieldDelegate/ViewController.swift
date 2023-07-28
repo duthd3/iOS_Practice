e9b0f2bfed123288d27485dcfb267f409b853657
duthd3 <duthd3@naver.com>
2023-07-28 17:08:07 +0900

    DelegatePattern

--- UITextFieldDelegate/UITextFieldDelegate/ViewController.swift
+++ UITextFieldDelegate/UITextFieldDelegate/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate { //채택작업
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var enteredLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClicked(_ sender: UIButton) {
+      //  enteredLabel.text = textField.text
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        enteredLabel.text = textField.text
+        return true
+    }
+    
 }
+
 
